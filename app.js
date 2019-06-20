0e2a4339f181190c6a105670a045b8d14371ee70
Austin Cliff <ajcliff95@gmail.com>
2019-06-20 13:09:19 -0400

    added mongo with working CRUD

--- app.js
+++ app.js
@@ -2,67 +2,72 @@
 const express = require('express');
 const PORT = 3000;
 const app = express('app');
+const bodyParser = require('body-parser');
+
+//Mongo Imports
+const mongoose = require('mongoose');
+const Note = require('./models/note');
+const connectionString = 'mongodb://localhost:27017/notes';
 
 //Required to send json in request body
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Mock data
-let notes = require('./notes');
+//Initiate connection to db
+mongoose.connect(connectionString, { useNewUrlParser: true }, () => {
+    console.log(`Connection at ${connectionString} successful...`);
+});
 
 //API Version
 const version = 'v1';
 const apiUri = `/api/${version}`;
 
 //GET NOTES
-app.get(`${apiUri}/getNotes`, (req, res) => {
-    res.send(notes);
+app.get(`${apiUri}/`, (req, res) => {
+    Note.find({}).then(note => {
+        res.json(note);
+    });
 });
 
 //GET NOTE BY ID
-app.get(`${apiUri}/getNoteById/:noteId`, (req, res) => {
-    let id = req.params.noteId;
-    if (!notes[id] == id) {
-        console.log('Note with the specified id does not exist')
-        res.send(notes)
-    } else {
-        let noteToSend = notes.filter(note => (note.noteId == id));
-        res.send(noteToSend);
-    }
+app.get(`${apiUri}/getNoteById`, (req, res) => {
+    Note.findById(req.body.id).then(note => {
+        res.json(note);
+    });
 });
 
 //GET NOTE BY TITLE
-app.get(`${apiUri}/getNoteByTitle/:title`, (req, res) => {
-    let title = req.params.title;
-    let noteToSend = notes.filter(note => (note.title == title));
-    res.send(noteToSend);
+app.get(`${apiUri}/getNoteByTitle`, (req, res) => {
+    Note.find({ title: req.body.title }).then(note => {
+        res.json(note);
+    });
 });
 
 //CREATE NOTE
 app.post(`${apiUri}/createNote`, (req, res) => {
-    notes.push(req.body);
-    res.sendStatus(200);
+    Note.create(
+        {
+            "title": req.body.title, "description": req.body.description, "dateCreated": Date()
+        }).then(note => {
+            res.json(note);
+        });
 });
 
-//UPDATE NOTE USE 
-/*
-indexOf is problematic for this use case, use findIndex
-*/
-app.put(`${apiUri}/updateNote/:noteId`, (req, res) => {
-    let id = req.params.noteId;
-    let i = notes.findIndex(note => note.noteId == id);
-    notes.splice(i,1,req.body);
-    console.log(i);
-    res.sendStatus(200);
+//UPDATE NOTE, seems to not be returning new note immediately
+app.put(`${apiUri}/updateNote`, (req, res) => {
+    Note.findByIdAndUpdate(req.body.id,
+        {
+            "title": req.body.title, "description": req.body.description
+        }).then(note => {
+            res.json(note);
+        });
 });
 
 //DELETE NOTE
-app.delete(`${apiUri}/deleteNote/:noteId`, (req, res) => {
-    let id = req.params.noteId;
-    notes = notes.filter(note => (note.noteId != id));
-    console.log(notes);
-    res.sendStatus(200);
+app.delete(`${apiUri}/deleteNote`, (req, res) => {
+    Note.findByIdAndDelete(req.body.id).then(note => {
+        res.json(note);
+    });
 });
 
 //App listener
